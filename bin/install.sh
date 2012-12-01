0874f4ec9d5a0f7467b9ab65eb8d0d0ef3b2553a
Techwraith <derickson@yammer-inc.com>
2012-11-30 18:00:47 -0800

    force symlinks on install

--- bin/install.sh
+++ bin/install.sh
@@ -43,14 +43,14 @@ curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | 
 ln -s ~/.dotfiles/zsh/zen.zsh-theme ~/.oh-my-zsh/themes/zen.zsh-theme
 
 # install custom zshrc and friends
-ln -s ~/.dotfiles/zsh/.zshrc ~/.zshrc
-ln -s ~/.dotfiles/zsh/.zsh_aliases ~/.zsh_aliases
-ln -s ~/.dotfiles/zsh/.zsh_exports ~/.zsh_exports
-ln -s ~/.dotfiles/zsh/.zsh_functions ~/.zsh_functions
+ln -sf ~/.dotfiles/zsh/.zshrc ~/.zshrc
+ln -sf ~/.dotfiles/zsh/.zsh_aliases ~/.zsh_aliases
+ln -sf ~/.dotfiles/zsh/.zsh_exports ~/.zsh_exports
+ln -sf ~/.dotfiles/zsh/.zsh_functions ~/.zsh_functions
 
 # load vimrc.local and vimrc.bundles.local
-ln -s ~/.dotfiles/vim/.vimrc.local
-ln -s ~/.dotfiles/vim/.vimrc.bundles.local
+ln -sf ~/.dotfiles/vim/.vimrc.local
+ln -sf ~/.dotfiles/vim/.vimrc.bundles.local
 
 # install spf13
 curl https://raw.github.com/spf13/spf13-vim/3.0/bootstrap.sh -L -o - | sh
