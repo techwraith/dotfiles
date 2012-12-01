18d8e326a543af9c5849c8bda3f9ab55f8c34160
Techwraith <derickson@yammer-inc.com>
2012-11-30 22:20:07 -0800

    add custom vim back in

--- bin/install.sh
+++ bin/install.sh
@@ -25,7 +25,7 @@ ln -sf ~/.dotfiles/vim/.vimrc.bundles.local ~/.vimrc.bundles.local
 
 # install spf13
 curl https://raw.github.com/spf13/spf13-vim/3.0/bootstrap.sh -L | sh
-#vim +BundleInstall! +BundleClean! +qall
+vim +BundleInstall! +BundleClean! +qall
 
 # source the shits
 source ~/.zshrc
