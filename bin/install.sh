ab4d757e00f8a46dec5ce2055b304728ace57e10
Daniel Erickson <techwraithpdx@gmail.com>
2012-11-30 21:25:58 -0800

    Update bin/install.sh

--- bin/install.sh
+++ bin/install.sh
@@ -26,3 +26,6 @@ ln -sf ~/.dotfiles/vim/.vimrc.bundles.local ~/.vimrc.bundles.local
 # install spf13
 curl https://raw.github.com/spf13/spf13-vim/3.0/bootstrap.sh -L -o - | sh
 vim +BundleInstall! +BundleClean! +qall
+
+# source the shits
+soruce ~/.zshrc
